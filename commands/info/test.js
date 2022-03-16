080d9f10058277d3665bcc50e8b57d83464ffd74
IDcLuc <lucativio@gmail.com>
2022-03-16 12:55:36 +0100

    initialize github repository

---
+++ commands/info/test.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "test",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        message.reply("yes working yesyesyesy")
+    }
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+let bot = {
+    client,
+    prefix: "g!",
+    owners: ["495514555615543329"]
+}
+
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot
+
+client.login(process.env.TOKEN)

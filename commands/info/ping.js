8e57deaa6d46a5a99eb79608c703962578a7c24f
IDcLuc <lucativio@gmail.com>
2022-03-16 16:07:03 +0100

    e

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -4,6 +4,6 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
-        message.reply("Pong! ``e``")
+        message.reply("Pong! " + `${Date.now() - message.createdTimestamp}` + "ms (yes I know there's a minus don't mind it)")
     }
 }

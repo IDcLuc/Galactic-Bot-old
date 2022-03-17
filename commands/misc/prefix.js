43e6ee5983d65e57da5a4e4f1474f9a1682c9c1a
IDcLuc <lucativio@gmail.com>
2022-03-17 16:45:27 +0100

    yes

--- commands/misc/prefix.js
+++ commands/misc/prefix.js
@@ -1,9 +1,11 @@
+const { MessageEmbed, Guild } = require("discord.js")
+
 module.exports = {
     name: "prefix",
     category: "misc",
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
-        message.reply(`The prefix for **${message.guild}** is *``g!``*.`)
+        message.reply("The prefix for *``" + message.guild.name + "``* is *``g!``*.")
     }
 }

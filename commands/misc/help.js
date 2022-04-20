cae05c6d4e0e41059789ede3145f157616803129
IDcLuc <lucativio@gmail.com>
2022-04-20 13:02:16 +0200

    .

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -15,9 +15,11 @@ module.exports = {
                 .setDescription(`The prefix for **${message.guild.name}** is **"g!"**. Use g!help [command] to get info about a specific command.`)
                 .addFields(
                     { name: "Misc", value: "help, ping, pong, prefix" },
-                    { name : "Moderation", value: "kick, ban, mute"}
+                    { name: "Moderation", value: "kick, ban, mute"},
+                    { name: "Fun", value: "confirmbald"},
+                    { name: "Search", value: "namemc, skycrypt"}
                 )
-                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
+                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
  
             let kickembed = new MessageEmbed()
             kickembed 
@@ -28,7 +30,7 @@ module.exports = {
                     { name: "Usage", value: "g!kick [member] [reason]"},
                     { name: "Example", value: "g!kick <@453943223229087748> Bullying"}
                 )
-                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
+                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
 
             let banembed = new MessageEmbed()
             banembed
@@ -39,7 +41,7 @@ module.exports = {
                     { name: "Usage", value: "g!ban [member] [reason]"},
                     { name: "Example", value: "g!ban <@453943223229087748> Raiding"}
                 )
-                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
+                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
             
             let muteembed = new MessageEmbed()
             muteembed
@@ -50,7 +52,7 @@ module.exports = {
                     { name: "Usage", value: "g!mute [member] [reason]"},
                     { name: "Example", value: "g!mute <@453943223229087748> Excessive usage of bad words"}
                 )
-                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
+                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
             
             let pingembed = new MessageEmbed()
             pingembed
@@ -61,7 +63,7 @@ module.exports = {
                     { name: "Usage", value: "g!ping"},
                     { name: "Alias", value: "g!pong"}
                 )
-                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
+                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
 
             let prefixembed = new MessageEmbed()
                 .setTitle("ping")
@@ -71,7 +73,7 @@ module.exports = {
                     { name: "Usage", value: "g!prefix"},
                     { name: "Alias", value: "g!pref"}
                 )
-                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
+                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
 
             let nmcembed = new MessageEmbed()
             nmcembed
@@ -84,7 +86,7 @@ module.exports = {
                     { name: "Alias", value: "g!nmc"},
                     { name: "Example", value: "g!namemc MajorX500 \n g!nmc IDcLuc"}
                 )
-                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
+                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
                 
             let statsembed = new MessageEmbed()
             statsembed
@@ -97,18 +99,18 @@ module.exports = {
                     { name: "Alias", value: "g!sb \n g!stats"},
                     { name: "Example", value: "g!skycrypt MajorX500 \n g!sb IDcLuc \n g!stats Observin"}
                 )
-                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})    
+                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})    
             
             let baldembed = new MessageEmbed()
             baldembed
-                .setTitle("Baldconfirm")
+                .setTitle("Confirmbald")
                 .setDescription("Confirms someone's baldness.")
                 .addFields(
                     { name: "Usage", value: "g!baldconfirm [name]"},
                     { name: "Alias", value: "g!bald \n g!confirmbald"},
                     { name: "usage", value: "g!baldconfirm Major \n g!bald Majorx500 \n g!confirmbald <@794258833924292608>"}
                 )
-                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
+                .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
         
         switch(cmdname){
         case ("kick"):
@@ -127,10 +129,10 @@ module.exports = {
             message.reply({ embeds: [prefixembed] })
         break;
         case ('pref'):
-            message.reply({ embeds: [embed] })
+            message.reply({ embeds: [prefixembed] })
         break;
         case ("pong"):
-            message.reply({ embeds: [ping] })
+            message.reply({ embeds: [pingembed] })
         break;
         case ("namemc" && "nmc"):
             message.reply({ embeds: [nmcembed]})
@@ -138,7 +140,7 @@ module.exports = {
         case ("skycrypt" && "sb" && "stats"):
             message.reply({ embeds: [statsembed] })
         break;
-        case ("bald" && "baldconfirm"):
+        case ("bald" && "baldconfirm" && "confirmbald"):
             message.reply({ embeds: [baldembed]} )
         break;
         default:
--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -18,7 +18,7 @@ module.exports = {
                 { name: "Usage", value: "g!mute [member] [reason]"},
                 { name: "Example", value: "g!mute <@453943223229087748> Excessive usage of bad words"}
             )
-            .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
+            .setFooter({ text: `Galactic Bot by IDcLuc ● Requested by ${message.author.tag}`, iconURL: "https://cdn.discordapp.com/avatars/952178870646366248/387f44e15d6eb3d51d5ebeddf0503937.webp?size=240"})
         return message.reply({ embeds: [embede] })
         }
 
@@ -29,21 +29,7 @@ module.exports = {
 
         let reasonn = args.slice(1).join(" ")
         let member = message.mentions.members.first()
-        let role = message.guild.roles.cache.find(r => r.id === "955550751575388190");
-
-        if(message.author.id === "266593502543085569"){
-            try {
-                await member.roles.add("Muted")
-                    if(args[1])
-                        message.reply("<@" + member + "> has been muted for ``" + reasonn +"``.")
-                    if(!args[1])
-                        return message.reply("<@" + member + "> has been muted.")
-            } catch (err) {
-                console.log (err)
-                message.reply("An error occured while performing this action.")
-            }
-        }
-        let memberr = message.mentions.members.first()
+        let role = message.guild.roles.cache.find(r => r.id === "955549898395222108");
 
         if(member.permissions.has(Permissions.FLAGS.KICK_MEMBERS) && !member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)){
             return message.reply(`You can't mute \`\`${member.user.username}\`\` as they have kick permissions.`)
@@ -52,12 +38,12 @@ module.exports = {
             let reason = args.slice(1).join(" ")
         try {
             if(args[1])
-            await memberr.send(`You have been muted from **${message.guild.name}** for *\`\`${reason}\`\`*!`)
+            await member.send(`You have been muted from **${message.guild.name}** for *\`\`${reason}\`\`*!`)
             else if(!args[1])
-            await memberr.send(`You have been muted from **${message.guild.name}**!`)
-            await member.roles.add("Muted")
+            await member.send(`You have been muted from **${message.guild.name}**!`)
+            await member.roles.add(role)
                 if(args[1])
-                    message.reply("<@" + member + "> has been muted for ``" + reasonn +"``.")
+                    message.reply(`${member.user.tag} has been muted for *\`\`${reasonn}\`\`*.`)
                 if(!args[1])
                     return message.reply("<@" + member + "> has been muted.")
         } catch (err) {

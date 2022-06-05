dd0cf45d791dcd844368a368fef55e1f6fc91110
IDcLuc <lucativio@gmail.com>
2022-06-06 00:13:19 +0200

    Added cooldown feature, so cool!

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-
+const cooldowns = new Map()
 module.exports = {
     name: "messageCreate",
     run: async function runAll(bot, message) {
@@ -17,6 +17,25 @@ module.exports = {
         let command = client.commands.get(cmdstr) || client.commands.find(a => a.aliases && a.aliases.includes(cmdstr))
 
         if (!command) return
+
+    if(!cooldowns.has(command.name)){
+        cooldowns.set(command.name, new Discord.Collection())
+    }       
+
+    const now = Date.now()
+    const timestamps = cooldowns.get(command.name)
+    const cooldownAmount = (command.cooldown) * 1000
+
+    if(timestamps.has(message.author.id)){
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount
+
+        if(now < expirationTime){
+            const timeLeft = (expirationTime - now) / 1000
+            return message.reply(`Please wait ${timeLeft.toFixed(1)} more seconds to use this command.`)
+        }
+    }
+
+    timestamps.set(message.author.id, now)
 
         let member = message.member
 

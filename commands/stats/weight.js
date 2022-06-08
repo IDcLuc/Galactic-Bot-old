8bb5a0116ee75944e2a81506fdacb63799e1085f
IDcLuc <lucativio@gmail.com>
2022-06-08 18:32:53 +0200

    Finally done with the weight command.. I think

--- commands/stats/weight.js
+++ commands/stats/weight.js
@@ -63,6 +63,7 @@ module.exports = {
                         tame: 500
                     }
                 }
+                
                 function calcSkillWeight(skillname, numidk) {
                     const percentage = skillname.xpCurrent/skillname.xpForNext * 100
                     const lev = skillname.level + percentage / 100
@@ -78,62 +79,107 @@ module.exports = {
                 let farm = calcSkillWeight(sbstat.skills.farming, skillWeig.exponents.farming)
                 let tame = calcSkillWeight(sbstat.skills.taming, skillWeig.exponents.taming)
 
-                function max_weight(skill, weight) {
-                    return skill > weight ? weight : skill
+                function max_skill(weight, maxweight) {
+                    return weight > maxweight ? maxweight : weight
                 }
 
-                forage = max_weight(forage, skillWeig.maxWeight.forage)
-                fish = max_weight(fish, skillWeig.maxWeight.fish)
-                brewing = max_weight(brewing, skillWeig.maxWeight.brewing)
-                enchant = max_weight(enchant, skillWeig.maxWeight.enchant)
-                fight = max_weight(fight, skillWeig.maxWeight.fight)
-                mine = max_weight(mine, skillWeig.maxWeight.mine)
-                farm = max_weight(farm, skillWeig.maxWeight.farm)
-                tame = max_weight(tame, skillWeig.maxWeight.tame)
-                // if(forage >= skillWeig.maxWeight.forage) forage = skillWeig.maxWeight.forage
-                // if(fish >= skillWeig.maxWeight.fish) fish = skillWeig.maxWeight.fish
-                // if(brewing >= skillWeig.maxWeight.brewing) brewing = skillWeig.maxWeight.brewing
-                // if(enchant >= skillWeig.maxWeight.enchant) enchant = skillWeig.maxWeight.enchant
-                // if(fight >= skillWeig.maxWeight.fight) fight = skillWeig.maxWeight.fight
-                // if(mine >= skillWeig.maxWeight.mine) mine = skillWeig.maxWeight.mine
-                // if(farm >= skillWeig.maxWeight.farm) farm = skillWeig.maxWeight.farm
-                // if(tame >= skillWeig.maxWeight.tame) tame = skillWeig.maxWeight.tame
+                forage = max_skill(forage, skillWeig.maxWeight.forage)
+                fish = max_skill(fish, skillWeig.maxWeight.fish)
+                brewing = max_skill(brewing, skillWeig.maxWeight.brewing)
+                enchant = max_skill(enchant, skillWeig.maxWeight.enchant)
+                fight = max_skill(fight, skillWeig.maxWeight.fight)
+                mine = max_skill(mine, skillWeig.maxWeight.mine)
+                farm = max_skill(farm, skillWeig.maxWeight.farm)
+                tame = max_skill(tame, skillWeig.maxWeight.tame)
 
-
-                const skillWeightt = Math.round((forage + fish + brewing + enchant + fight + mine + farm + tame) * 10) / 10
+                const skillWeightt = forage + fish + brewing + enchant + fight + mine + farm + tame
 
                 // dungeons weight
+                const dungeonExponents = {
+                    dividers: {
+                        catacombs: 0.0002149604615,
+                        healer: 0.0000045254834,
+                        mage: 0.0000045254834,
+                        berserk: 0.0000045254834,
+                        archer: 0.0000045254834,
+                        tank: 0.0000045254834,                                              
+                    },
+                    maxWeight: {
+                        catacombs: 9500
+                    }
+                }
                 function calcDungeonWeight(type, divider) {
                     const percentage = type.xpCurrent/type.xpForNext * 100
                     const lev = type.level + percentage / 100
                     return Math.pow(lev, 4.5) * divider
                 }
-                const catacombs = calcDungeonWeight(sbstat.dungeons.types.catacombs, 0.0002149604615)
-                const healer = calcDungeonWeight(sbstat.dungeons.classes.healer, 0.0000045254834)
-                const berserk = calcDungeonWeight(sbstat.dungeons.classes.berserk, 0.0000045254834)
-                const archer = calcDungeonWeight(sbstat.dungeons.classes.archer, 0.0000045254834)
-                const mage = calcDungeonWeight(sbstat.dungeons.classes.mage, 0.0000045254834)
-                const tank = calcDungeonWeight(sbstat.dungeons.classes.tank, 0.0000045254834)
+                let catacombs = calcDungeonWeight(sbstat.dungeons.types.catacombs, dungeonExponents.dividers.catacombs)
+                let healer = calcDungeonWeight(sbstat.dungeons.classes.healer, dungeonExponents.dividers.healer)
+                let berserk = calcDungeonWeight(sbstat.dungeons.classes.berserk, dungeonExponents.dividers.berserk)
+                let archer = calcDungeonWeight(sbstat.dungeons.classes.archer, dungeonExponents.dividers.archer)
+                let mage = calcDungeonWeight(sbstat.dungeons.classes.mage, dungeonExponents.dividers.mage)
+                let tank = calcDungeonWeight(sbstat.dungeons.classes.tank, dungeonExponents.dividers.tank)
                 
-                const dw = Math.round((catacombs + healer + berserk + archer + mage + tank) * 10) / 10
+                function max_dungeon(weight, maxweight) {   
+                    return weight > maxweight ? maxweight : weight
+                }
+                catacombs = max_dungeon(catacombs) //, skillWeig.maxWeight.catacombs
+                healer = max_dungeon(healer) //, skillWeig.maxWeight.healer
+                berserk = max_dungeon(berserk) //, skillWeig.maxWeight.berserk
+                archer = max_dungeon(archer) //, skillWeig.maxWeight.archer
+                mage = max_dungeon(mage) //, skillWeig.maxWeight.mage
+                tank = max_dungeon(tank) //, skillWeig.maxWeight.tank
+
+                function max_dungeons(weight, maxweight) {
+                    return weight > maxweight ? maxweight : weight
+                }
+                
+                catacombs = max_dungeon(catacombs, dungeonExponents.maxWeight.catacombs)
+
+                const dw = catacombs + healer + berserk + archer + mage + tank
 
                 // slayer weight
+                const slayerExponents = {
+                    dividers: {
+                        wolf: 1962,
+                        rev: 2208,
+                        tara: 2118,
+                        enderman: 1430 
+                    },
+                    maxWeight: {
+                        wolf: 509.7,
+                        tara: 472.1,
+                        enderman: 699.3,
+                        rev: 452.9
+                    }
+                }
                 function calcSlayerWeight(type, divider) {
                     return type.xp / divider
                 }
-                const rev = calcSlayerWeight(sbstat.slayer.zombie, 2208)
-                const tara = calcSlayerWeight(sbstat.slayer.spider, 2118)
-                const wolf = calcSlayerWeight(sbstat.slayer.wolf, 1962)
-                const enderman = calcSlayerWeight(sbstat.slayer.enderman, 1430)
+                let rev = calcSlayerWeight(sbstat.slayer.zombie, slayerExponents.dividers.rev)
+                let tara = calcSlayerWeight(sbstat.slayer.spider, slayerExponents.dividers.tara)
+                let wolf = calcSlayerWeight(sbstat.slayer.wolf, slayerExponents.dividers.wolf)
+                let enderman = calcSlayerWeight(sbstat.slayer.enderman, slayerExponents.dividers.enderman)
 
-                const slw = Math.round((rev + tara + wolf + enderman) * 10) / 10
-                const senWeight = slw + skillWeightt + dw
+                function max_slayer(weight, maxweight) {
+                    return weight > maxweight ? maxweight : weight
+                }
+                rev = max_slayer(rev, slayerExponents.maxWeight.rev)
+                tara = max_slayer(tara, slayerExponents.maxWeight.tara)
+                wolf = max_slayer(wolf, slayerExponents.maxWeight.wolf)
+                enderman = max_slayer(enderman, slayerExponents.maxWeight.enderman)
+
+                const slw = rev + tara + wolf + enderman
+
+                const senWeight = Math.round((slw + skillWeightt + dw) * 10) / 10
 
                 function between(min, max, num) {
                     return num >= min && num < max
                 }
-                let stage = "Early Game";
-                switch (true) {  //get stage
+
+                //get stage
+                let stage;
+                switch (true) {
                     case between(2000, 7000, senWeight): 
                         stage = "Mid Game"
                     break;
@@ -149,20 +195,50 @@ module.exports = {
                     case senWeight >= 30000:
                         stage = "What the fuck.";
                     break;
+                    default: "Early Game";
                 }
 
+                //formating
+                const senWeightFormated = new Intl.NumberFormat('en-US').format(senWeight)
+
+                const farmFormated = new Intl.NumberFormat('en-US').format(Math.round(farm * 10) / 10)
+                const mineFormated = new Intl.NumberFormat('en-US').format(Math.round(mine * 10) / 10)
+                const forageFormated = new Intl.NumberFormat('en-US').format(Math.round(forage * 10) / 10)
+                const fishFormated = new Intl.NumberFormat('en-US').format(Math.round(fish * 10) / 10)
+                const brewingFormated = new Intl.NumberFormat('en-US').format(Math.round(brewing * 10) / 10)
+                const enchantFormated = new Intl.NumberFormat('en-US').format(Math.round(enchant * 10) / 10)
+                const fightFormated = new Intl.NumberFormat('en-US').format(Math.round(fight * 10) / 10)
+                const tameFormated = new Intl.NumberFormat('en-US').format(Math.round(tame * 10) / 10)
+                
+                const catacombsFormated = new Intl.NumberFormat('en-US').format(Math.round(catacombs * 10) / 10)
+                const healerFormated = new Intl.NumberFormat('en-US').format(Math.round(healer * 10) / 10)
+                const berserkFormated = new Intl.NumberFormat('en-US').format(Math.round(berserk * 10) / 10)
+                const archerFormated = new Intl.NumberFormat('en-US').format(Math.round(archer * 10) / 10)
+                const mageFormated = new Intl.NumberFormat('en-US').format(Math.round(mage * 10) / 10)
+                const tankFormated = new Intl.NumberFormat('en-US').format(Math.round(tank * 10) / 10)
+
+                const revFormated = new Intl.NumberFormat('en-US').format(Math.round(rev * 10) / 10)
+                const taraFormated = new Intl.NumberFormat('en-US').format(Math.round(tara * 10) / 10)
+                const wolfFormated = new Intl.NumberFormat('en-US').format(Math.round(wolf * 10) / 10)
+                const endermanFormated = new Intl.NumberFormat('en-US').format(Math.round(enderman * 10) / 10)
+
+                const dungeonFormated = new Intl.NumberFormat('en-US').format(Math.round(dw * 10) / 10)
+                const slayerFormated = new Intl.NumberFormat('en-US').format(Math.round(slw * 10) / 10)
+                const skillFormated = new Intl.NumberFormat('en-US').format(Math.round(skillWeightt * 10) / 10)
+
+
+                //create and send embed 
                 let theEmbed = new MessageEmbed()
-                    .setTitle(`${playerObj}'s Senither Weight on ${lastprofile} without Overflow`)
-                    .setURL(`https://sky.shiiyu.moe/${plr}`)
-                    .setDescription(`Total: **${Math.round((senWeight) * 10) / 10}**\n Stage: **${stage}**`)
+                    .setTitle(`${playerObj}'s Senither Weight on ${lastprofile} (without Overflow)`)
+                    .setURL(`https://sky.shiiyu.moe/stats/${plr}`)
+                    .setDescription(`Total: **${senWeightFormated}**\n Stage: **${stage}**`)
                     .setThumbnail(`https://crafatar.com/renders/body/${playerID}?overlay&size=128`)
                     .addFields(
-                        { name: `<:diamond_sword:979322481678639124> Skills: ${skillWeightt}`, value:`➜ Farming: **${Math.round(farm * 10) / 10}**\n➜ Mining: **${Math.round(mine * 10) / 10}**\n➜ Foraging: **${Math.round(forage * 10) / 10}**\n➜ Combat: **${Math.round(fight * 10) / 10}**\n➜ Taming: **${Math.round(tame * 10) / 10}**\n➜ Fishing: **${Math.round(fish * 10) / 10}**\n➜ Alchemy: **${Math.round(brewing * 10) / 10}**\n➜ Enchanting: **${Math.round(enchant * 10) / 10}**`},
-                        { name: `<:maddox:979377063842676786> Slayers: ${slw}`, value: `➜ Revenant Horror: **${Math.round(rev * 10) / 10 }**\n➜ Tarantula Broodfather: **${Math.round(tara * 10) / 10}**\n➜ Sven Packmaster: **${Math.round(wolf * 10) / 10}**\n➜ Voidgloom Seraph: **${Math.round(enderman * 10) / 10}**`},
-                        { name: `<:catacombs:979377305073877072> Dungeons: ${dw}`, value: `➜ Catacombs: **${Math.round(catacombs * 10) / 10}**\n➜ Healer: **${Math.round(healer * 10) / 10}**\n➜ Berserker: **${Math.round(berserk * 10) / 10}**\n➜ Archer: **${Math.round(archer * 10) / 10}**\n➜ Tank: **${Math.round(tank * 10) / 10}**\n➜ Mage: **${Math.round(mage * 10) / 10}**`}
+                        { name: `<:diamond_sword:979322481678639124> Skills: ${skillFormated}`, value:`➜ Farming: **${farmFormated}**\n➜ Mining: **${mineFormated}**\n➜ Foraging: **${forageFormated}**\n➜ Combat: **${fightFormated}**\n➜ Taming: **${tameFormated}**\n➜ Fishing: **${fishFormated}**\n➜ Alchemy: **${brewingFormated}**\n➜ Enchanting: **${enchantFormated}**`},
+                        { name: `<:maddox:979377063842676786> Slayers: ${slayerFormated}`, value: `➜ Revenant Horror: **${revFormated}**\n➜ Tarantula Broodfather: **${taraFormated}**\n➜ Sven Packmaster: **${wolfFormated}**\n➜ Voidgloom Seraph: **${endermanFormated}**`},
+                        { name: `<:catacombs:979377305073877072> Dungeons: ${dungeonFormated}`, value: `➜ Catacombs: **${catacombsFormated}**\n➜ Healer: **${healerFormated}**\n➜ Berserker: **${berserkFormated}**\n➜ Archer: **${archerFormated}**\n➜ Tank: **${tankFormated}**\n➜ Mage: **${mageFormated}**`}
                     )
                     .setFooter({ text: `Galactic Bot Stats ● Requested by ${message.author.tag}`, iconURL: client.user.displayAvatarURL()})
-
 
                 message.reply({ embeds: [theEmbed]} )
             }).catch(e => {

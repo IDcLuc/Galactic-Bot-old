11a68eda9cdfa2a89a2326f609691209bf2181b8
IDcLuc <lucativio@gmail.com>
2022-06-09 20:22:09 +0200

    Added meme command

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,11 +1,12 @@
 const fetch = require('node-fetch')
 const { MessageEmbed } = require('discord.js')
+
 module.exports = {
     name: "meme",
     category: "fun",
     permissions: [],
     devOnly: false,
-    run: async (client, message, args) => {
+    run: async ({client, message, args}) => {
         function getMeme() {
             return fetch(`https://some-random-api.ml/meme`)
             .then(data => data.json())
@@ -14,11 +15,12 @@ module.exports = {
 
         const [meme, caption] = await getMeme();
 
-        let memeembed = new MessageEmbed()
+        const memeembed = new MessageEmbed()
         memeembed
             .setTitle(`${caption}`)
             .setImage(`${meme}`)
-            .setFooter({ text: `Galactic Bot ● Powered by some-random-api.ml ● Requested by ${message.author.tag}`, iconURL: client.user.displayAvatarURL()})
-        message.channel.send(memeembed)
+            .setColor("RANDOM")
+            .setFooter({ text: `Galactic Bot by IDcLuc ● Powered by some-random-api.ml ● Requested by ${message.author.tag}`, iconURL: client.user.displayAvatarURL()})
+        message.reply({ embeds: [memeembed] })
     }
 }

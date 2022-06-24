880ad893924e6024f462bf98212be466fb4dba69
IDcLuc <lucativio@gmail.com>
2022-06-24 13:20:38 +0200

    Added a 15 seconds wait time

--- message-counter.js
+++ message-counter.js
@@ -2,16 +2,20 @@ const mongo = require("mongoose")
 const schema = require("./schemas/rank/message-count-schema")
 
 module.exports = client => {
-    
     client.on('messageCreate', async (message) => {    
 
-        if(message.author.bot) return;
-        if(message.channel.type === 'dm') return;
+        if(message.author.bot) return
+        if(message.channel.type === 'dm') return
         if(message.content.startsWith('g!')) return
-
+        
+        const messages = await message.channel.messages.fetch()
+        const userMessages = await messages.filter(m => m.author.id === message.author.id)
+        const userLastMessage = userMessages.first(2)[1]
+        if (userLastMessage && Math.abs(userLastMessage.createdAt - message.createdAt) < 15000) return
+        
         let member = message.author.id
         let dataQuery = await schema.findOne({ userID: member, guildID: message.guild.id })
-
+        
         if (!dataQuery) {
             const newSchem = new schema(
                 {
@@ -24,7 +28,6 @@ module.exports = client => {
                     upsert: true
                 }
             )
-        
             await newSchem.save()
         }
         else {
